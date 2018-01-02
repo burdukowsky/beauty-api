40d98400216497b1fd49a1d9253b43db0bbda472
burdukowsky <burdukowskystas@gmail.com>
2018-01-02 19:51:02 +0700

    add spring boot

---
+++ src/main/java/tk/burdukowsky/beauty_api/BeautyApiApplication.java
@@ -0,0 +1,12 @@
+package tk.burdukowsky.beauty_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeautyApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeautyApiApplication.class, args);
+    }
+}
