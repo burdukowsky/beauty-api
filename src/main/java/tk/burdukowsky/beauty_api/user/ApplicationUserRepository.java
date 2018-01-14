68cb61d3498979e8211aa23274ab062713e959e1
burdukowsky <burdukowskystas@gmail.com>
2018-01-14 13:15:23 +0700

    change API users path

--- src/main/java/tk/burdukowsky/beauty_api/user/ApplicationUserRepository.java
+++ src/main/java/tk/burdukowsky/beauty_api/user/ApplicationUserRepository.java
@@ -1,7 +1,9 @@
 package tk.burdukowsky.beauty_api.user;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface ApplicationUserRepository extends PagingAndSortingRepository<ApplicationUser, Long> {
 
     ApplicationUser findByEmail(String email);
