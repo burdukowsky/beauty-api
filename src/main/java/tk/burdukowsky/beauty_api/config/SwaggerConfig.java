30b4cbb9f438b5c6319914d31d18dcfd57ccb7f3
burdukowsky <burdukowskystas@gmail.com>
2018-02-02 13:36:21 +0700

    add swagger ApiInfo

--- src/main/java/tk/burdukowsky/beauty_api/config/SwaggerConfig.java
+++ src/main/java/tk/burdukowsky/beauty_api/config/SwaggerConfig.java
@@ -3,8 +3,11 @@ package tk.burdukowsky.beauty_api.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -18,9 +21,21 @@ public class SwaggerConfig {
     @Bean
     public Docket api() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(apiInfo())
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build();
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("API for the project \"Advisory service for choosing a beauty salon\"")
+                .description("API for the project \"Advisory service for choosing a beauty salon\"")
+                .contact(new Contact("Stanislav Burdukowsky", "http://burdukowsky.tk", "burdukowskystas@gmail.com"))
+                .license("MIT License")
+                .licenseUrl("https://github.com/burdukowsky/beauty-api/blob/master/LICENSE")
+                .version("1.0.0")
                 .build();
     }
 }
