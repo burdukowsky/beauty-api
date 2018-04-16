d08f1ebb70a62d30f7e1e8eb16b62d5a38c09f20
burdukowsky <burdukowskystas@gmail.com>
2018-04-16 22:06:34 +0700

    add findAllByRolesName method to ApplicationUserRepository

--- src/main/java/tk/burdukowsky/beauty_api/user/ApplicationUserRepository.java
+++ src/main/java/tk/burdukowsky/beauty_api/user/ApplicationUserRepository.java
@@ -1,10 +1,17 @@
 package tk.burdukowsky.beauty_api.user;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.security.access.prepost.PreAuthorize;
+
+import java.util.List;
 
 @RepositoryRestResource(collectionResourceRel = "users", path = "users")
 public interface ApplicationUserRepository extends PagingAndSortingRepository<ApplicationUser, Long> {
 
     ApplicationUser findByEmail(String email);
+
+    @PreAuthorize("hasRole('ADMIN')")
+    List<ApplicationUser> findAllByRolesName(@Param("role") RoleEnum roleEnum);
 }
