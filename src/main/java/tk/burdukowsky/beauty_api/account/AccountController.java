c28fc2b70328154a5e86a363b3e693c743614912
burdukowsky <burdukowskystas@gmail.com>
2018-05-08 21:04:56 +0700

    add updateAccount method to AccountController

--- src/main/java/tk/burdukowsky/beauty_api/account/AccountController.java
+++ src/main/java/tk/burdukowsky/beauty_api/account/AccountController.java
@@ -3,19 +3,22 @@ package tk.burdukowsky.beauty_api.account;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.Authentication;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.bind.annotation.*;
 import tk.burdukowsky.beauty_api.user.ApplicationUser;
 import tk.burdukowsky.beauty_api.user.ApplicationUserRepository;
+import tk.burdukowsky.beauty_api.utils.ApplicationUtils;
 
 @RestController
 @RequestMapping("/account")
 public class AccountController {
     private ApplicationUserRepository applicationUserRepository;
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-    public AccountController(ApplicationUserRepository applicationUserRepository) {
+    public AccountController(ApplicationUserRepository applicationUserRepository,
+                             BCryptPasswordEncoder bCryptPasswordEncoder) {
         this.applicationUserRepository = applicationUserRepository;
+        this.bCryptPasswordEncoder = bCryptPasswordEncoder;
     }
 
     @PreAuthorize("isAuthenticated()")
@@ -26,5 +29,21 @@ public class AccountController {
             return ResponseEntity.notFound().build();
         }
         return ResponseEntity.ok().body(currentUser);
+    }
+
+    @PatchMapping
+    public ResponseEntity<ApplicationUser> updateAccount(@RequestBody ApplicationUser user,
+                                                         Authentication authentication) throws Exception {
+        ApplicationUser currentUser = applicationUserRepository.findByEmail((String) authentication.getPrincipal());
+        if (currentUser == null) {
+            return ResponseEntity.notFound().build();
+        }
+        user.setId(currentUser.getId());
+        if (user.getPassword() != null) {
+            user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+        }
+        ApplicationUser updatedUser = applicationUserRepository
+                .save(ApplicationUtils.merge(user, currentUser, ApplicationUser.class));
+        return ResponseEntity.ok().body(updatedUser);
     }
 }
