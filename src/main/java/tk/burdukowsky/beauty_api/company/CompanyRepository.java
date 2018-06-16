2f056566aca9fc86bd22975d8b453e42bd0e7bfa
burdukowsky <burdukowskystas@gmail.com>
2018-06-16 15:46:53 +0700

    remove querydsl from project

--- src/main/java/tk/burdukowsky/beauty_api/company/CompanyRepository.java
+++ src/main/java/tk/burdukowsky/beauty_api/company/CompanyRepository.java
@@ -3,7 +3,6 @@ package tk.burdukowsky.beauty_api.company;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
-import org.springframework.data.querydsl.QueryDslPredicateExecutor;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
@@ -15,8 +14,7 @@ import java.util.List;
 
 @PreAuthorize("isAuthenticated()")
 @RepositoryRestResource(collectionResourceRel = "companies", path = "companies")
-public interface CompanyRepository
-        extends PagingAndSortingRepository<Company, Long>, QueryDslPredicateExecutor<Company> {
+public interface CompanyRepository extends PagingAndSortingRepository<Company, Long> {
 
     // неэкспортируемые методы НАЧАЛО
 
