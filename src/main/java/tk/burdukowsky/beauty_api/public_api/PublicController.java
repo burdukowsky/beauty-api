446edd3ee25a20d28098c177584b0f78491cceff
burdukowsky <burdukowskystas@gmail.com>
2018-06-16 15:37:21 +0700

    implement 'companies-by-category-ids' GET-query

--- src/main/java/tk/burdukowsky/beauty_api/public_api/PublicController.java
+++ src/main/java/tk/burdukowsky/beauty_api/public_api/PublicController.java
@@ -23,7 +23,7 @@ public class PublicController {
     private EntityManager entityManager;
 
     @GetMapping("/companies-by-service-ids")
-    public ResponseEntity<CustomPage<Company>> getCompanies(
+    public ResponseEntity<CustomPage<Company>> getCompaniesByServiceIds(
             @RequestParam(required = false, defaultValue = "0") long page,
             @RequestParam(required = false, defaultValue = "20") long limit,
             @RequestParam(required = false, defaultValue = "name") CompanySortField sort,
@@ -33,42 +33,38 @@ public class PublicController {
 
         StringBuilder sbQuery = new StringBuilder();
 
-        sbQuery.append(" select c.* from companies c ");
+        appendSelectClauseToSelectCompanies(sbQuery);
 
-        appendWhereClause(sbQuery, type);
+        appendWhereClauseToSelectCompaniesByServiceIdsSql(sbQuery, type);
 
-        sbQuery.append(" order by ")
-                .append(sort.toString())
-                .append(sortDirection.equalsIgnoreCase("desc") ? " desc " : " asc ")
-                .append(" limit :limit offset :offset ");
+        appendOrderByAndLimitAndOffsetClauses(sbQuery, sort, sortDirection);
 
         Query query = entityManager.createNativeQuery(sbQuery.toString(), Company.class);
 
-        setParametersToQuery(query, ids, type);
+        setIdsAndTypeParametersToQuery(query, ids, type);
 
-        query.setParameter("limit", limit);
-        query.setParameter("offset", limit * page);
+        setLimitAndOffsetParametersToQuery(query, page, limit);
 
         @SuppressWarnings("unchecked")
         List<Company> data = query.getResultList();
 
         StringBuilder sbQueryTotal = new StringBuilder();
 
-        sbQueryTotal.append(" select count(c.id) from companies c ");
+        appendSelectClauseToSelectCompaniesCount(sbQueryTotal);
 
-        appendWhereClause(sbQueryTotal, type);
+        appendWhereClauseToSelectCompaniesByServiceIdsSql(sbQueryTotal, type);
 
         Query queryTotal = entityManager.createNativeQuery(sbQueryTotal.toString());
 
-        setParametersToQuery(queryTotal, ids, type);
+        setIdsAndTypeParametersToQuery(queryTotal, ids, type);
 
         long totalElements = ((BigInteger) queryTotal.getSingleResult()).longValue();
-        long totalPages = totalElements % limit == 0 ? totalElements / limit : totalElements / limit + 1;
 
-        return ResponseEntity.ok().body(new CustomPage<>(data, limit, totalElements, totalPages, page));
+        return ResponseEntity.ok()
+                .body(new CustomPage<>(data, limit, totalElements, calculateTotalPages(totalElements, limit), page));
     }
 
-    private void appendWhereClause(StringBuilder sb, CompanyType type) {
+    private void appendWhereClauseToSelectCompaniesByServiceIdsSql(StringBuilder sb, CompanyType type) {
         sb.append(" where cast(string_to_array(:ids, ',') as bigint []) <@ array(select cs.service_id ")
                 .append(" from companies_services cs where cs.company_id = c.id) ");
 
@@ -77,7 +73,7 @@ public class PublicController {
         }
     }
 
-    private void setParametersToQuery(Query query, List<Long> ids, CompanyType type) {
+    private void setIdsAndTypeParametersToQuery(Query query, List<Long> ids, CompanyType type) {
         query.setParameter("ids", ids.stream()
                 .filter(Objects::nonNull)
                 .map(Object::toString).collect(Collectors.joining(",")));
@@ -85,5 +81,80 @@ public class PublicController {
         if (type != null) {
             query.setParameter("companyType", type.name());
         }
+    }
+
+    @GetMapping("/companies-by-category-ids")
+    public ResponseEntity<CustomPage<Company>> getCompaniesByCategoryIds(
+            @RequestParam(required = false, defaultValue = "0") long page,
+            @RequestParam(required = false, defaultValue = "20") long limit,
+            @RequestParam(required = false, defaultValue = "name") CompanySortField sort,
+            @RequestParam(required = false, defaultValue = "asc") String sortDirection,
+            @RequestParam(required = false) CompanyType type,
+            @RequestParam List<Long> ids) {
+        StringBuilder sbQuery = new StringBuilder();
+
+        appendSelectClauseToSelectCompanies(sbQuery);
+
+        appendWhereClauseToSelectCompaniesByCategoryIdsSql(sbQuery, type);
+
+        appendOrderByAndLimitAndOffsetClauses(sbQuery, sort, sortDirection);
+
+        Query query = entityManager.createNativeQuery(sbQuery.toString(), Company.class);
+
+        setIdsAndTypeParametersToQuery(query, ids, type);
+
+        setLimitAndOffsetParametersToQuery(query, page, limit);
+
+        @SuppressWarnings("unchecked")
+        List<Company> data = query.getResultList();
+
+        StringBuilder sbQueryTotal = new StringBuilder();
+
+        appendSelectClauseToSelectCompaniesCount(sbQueryTotal);
+
+        appendWhereClauseToSelectCompaniesByCategoryIdsSql(sbQueryTotal, type);
+
+        Query queryTotal = entityManager.createNativeQuery(sbQueryTotal.toString());
+
+        setIdsAndTypeParametersToQuery(queryTotal, ids, type);
+
+        long totalElements = ((BigInteger) queryTotal.getSingleResult()).longValue();
+
+        return ResponseEntity.ok()
+                .body(new CustomPage<>(data, limit, totalElements, calculateTotalPages(totalElements, limit), page));
+    }
+
+    private void appendWhereClauseToSelectCompaniesByCategoryIdsSql(StringBuilder sb, CompanyType type) {
+        sb.append(" where cast(string_to_array(:ids, ',') as bigint []) <@ array(select distinct s.category_id ")
+                .append(" from companies_services cs left join services s on cs.service_id = s.id ")
+                .append(" where cs.company_id = c.id) ");
+
+        if (type != null) {
+            sb.append(" and c.company_type = :companyType ");
+        }
+    }
+
+    private void appendOrderByAndLimitAndOffsetClauses(StringBuilder sb, CompanySortField sort, String sortDirection) {
+        sb.append(" order by ")
+                .append(sort.toString())
+                .append(sortDirection.equalsIgnoreCase("desc") ? " desc " : " asc ")
+                .append(" limit :limit offset :offset ");
+    }
+
+    private void setLimitAndOffsetParametersToQuery(Query query, long page, long limit) {
+        query.setParameter("limit", limit);
+        query.setParameter("offset", limit * page);
+    }
+
+    private long calculateTotalPages(long totalElements, long limit) {
+        return totalElements % limit == 0 ? totalElements / limit : totalElements / limit + 1;
+    }
+
+    private void appendSelectClauseToSelectCompanies(StringBuilder sb) {
+        sb.append(" select c.* from companies c ");
+    }
+
+    private void appendSelectClauseToSelectCompaniesCount(StringBuilder sb) {
+        sb.append(" select count(c.id) from companies c ");
     }
 }
